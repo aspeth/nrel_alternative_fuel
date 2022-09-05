e8ce0db3c77d3771e312353e596e2085f0a4d5a7
Andrew Speth <speth.andrew@yahoo.com>
2022-09-05 09:40:17 -0600

    feat: API connection established in controller, returns nearby stations

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -1,5 +1,9 @@
 class StationsController < ApplicationController
   def index
+    conn = Faraday.new("https://developer.nrel.gov/")
+    response = conn.get("/api/alt-fuel-stations/v1/nearest?format=json&api_key=#{ENV["nrel_api_key"]}&location=#{params[:zipcode]}")
+    hash_response = JSON.parse(response.body, symbolize_names: :true)
     
+    @stations = hash_response[:fuel_stations]
   end
 end 
