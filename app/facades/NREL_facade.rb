2d4a29ffebd5b10d5bd8d1f076834a485f15b266
Neil Lambert <nlambert@doximity.com>
2022-09-09 10:30:17 -0700

    code organization commentary

--- app/facades/NREL_facade.rb
+++ app/facades/NREL_facade.rb
@@ -1,3 +1,13 @@
+# I wouldn't create a `facade` folder nor use that terminology here.
+# Generally we want to keep code together in little piles wherever we can, so I would do:
+# app/
+#   services/
+#     nrel/
+#       service.rb
+#       api_client.rb
+# NRELFacade => Nrel::Service
+# NRELService => Nrel::ApiClient
+#
 class NRELFacade
   def self.get_stations(location_string)
     location = Location.find_or_create_by(location_string: location_string)
@@ -8,10 +18,10 @@ class NRELFacade
       stations.map do |station|
         location.stations << Station.create!(name: station[:station_name], address: "#{station[:street_address]} #{station[:city]}, #{station[:state]} #{station[:zip]}", location_id: location.id, access_code: station[:access_code])
       end
-      
+
       location.save!
     end
-    
+
     location.stations
   end
 end
