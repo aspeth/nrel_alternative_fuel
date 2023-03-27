ad04e843eee03a2ee7636639764e7731d8f78a6c
Andrew Speth <speth.andrew@yahoo.com>
2023-03-27 07:58:34 -0600

    use namespacing for code organization

--- app/services/NREL_service.rb
+++
@@ -1,7 +0,0 @@
-class NRELService
-  def self.get_stations(location_string)
-    conn = Faraday.new("https://developer.nrel.gov/")
-    response = conn.get("/api/alt-fuel-stations/v1/nearest?format=json&api_key=#{ENV["nrel_api_key"]}&location=#{location_string}")
-    hash_response = JSON.parse(response.body, symbolize_names: :true)[:fuel_stations]
-  end
-end
